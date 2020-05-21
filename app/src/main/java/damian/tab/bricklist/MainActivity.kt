bc0206086126a052bfd60783fa19b062e4101cb9
damianTab <dam.tab@wp.pl>
2020-05-21 09:49:03 +0200

    add simple tooblar

--- app/src/main/java/damian/tab/bricklist/MainActivity.kt
+++ app/src/main/java/damian/tab/bricklist/MainActivity.kt
@@ -1,12 +1,34 @@
 package damian.tab.bricklist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        println(item.itemId)
+        return super.onOptionsItemSelected(item)
     }
 }
